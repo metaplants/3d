16af8fdf091a9739d081de027e083cb2d47c870a
h.higuchi.93@gmail.com <h.higuchi.93@gmail.com>
2022-11-18 03:43:53 +0900

    video2image追加

---
+++ utils/video2image.py
@@ -0,0 +1,32 @@
+import cv2
+from matplotlib import pyplot as plt
+
+def video2image(video_path, image_dir_path, skip_rate=10, show=False, write=True):
+    i = 0
+    cap = cv2.VideoCapture(video_path)
+    while(1):
+        ret, frame = cap.read()
+        if ret==False:
+            break
+        if(i % skip_rate == 0):
+            j = int(i/skip_rate)   
+            if write:            
+                image_path = f"{image_dir_path}/{j}.png" 
+                print(f"Saving frame {i} to {image_path}")
+                cv2.imwrite(image_path, frame)
+            if show:        
+                plt.imshow(frame)
+                plt.show()
+        i += 1
+    cap.release()
+
+
+if __name__ =="__main__":
+    args = {
+        "video_path": "/Users/higuchi/dev/Metaplants/3d/data/test.mp4",
+        "image_dir_path": "/Users/higuchi/dev/Metaplants/3d/data/test",
+        "skip_rate": 200,
+        "show": True,
+        "write": False,      
+    }
+    video2image(**args)
